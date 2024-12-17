77d68649878bd4f88a924232213851ffc7ab7f7c
Marko <markobattlenet@gmail.com>
2024-12-17 12:38:22 +0100

    HandlingMultipleSelectDropdown
    Completed

---
+++ tests/HandleMultiSelectDropdown.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from "@playwright/test";
+
+test('Handle Multi select dropdown', async ({ page }) => {
+    await page.goto("https://demoqa.com/select-menu");
+    
+    // single item in array
+    // await page.locator("#cars").selectOption(['Opel'])
+
+
+    // multiple select with visible text
+    // await page.locator("#cars").selectOption(['Volvo', 'Opel', 'Audi']);
+
+    // multiple select with value and visible text
+    await page.locator("#cars").selectOption(['volvo', 'Opel', 'Audi']);
+})
