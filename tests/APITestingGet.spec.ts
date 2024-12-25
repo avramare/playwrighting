0a0488f0a7f7e4a83a939b3b5354ad40c27a4cb5
Marko <markobattlenet@gmail.com>
2024-12-25 13:57:02 +0100

    Different ways to pass baseURL

--- tests/APITestingGet.spec.ts
+++ tests/APITestingGet.spec.ts
@@ -1,5 +1,15 @@
 import { test, request } from "@playwright/test";
 
+// 3. baseURL before all
+let reqContext2
+test.beforeAll("Before all the tests...", async () => {
+    reqContext2 = await request.newContext({
+        baseURL: "https://restful-booker.herokuapp.com"
+    })
+})
+
+
+// 1.baseURL passed to http method, API RequestContext
 test('API testing GET practice 1', async ({ request }) => {
 
     const response = await request.get("https://restful-booker.herokuapp.com/booking");
@@ -7,13 +17,30 @@ test('API testing GET practice 1', async ({ request }) => {
 
 })
 
+// 2.baseURL passed with NewContext , API Request
 test('API testing GET practice 2', async () => {
     const reqContext = await request.newContext({
         baseURL: "https://restful-booker.herokuapp.com"
     });
-    const response1 = await reqContext.get("/booking");
-    console.log(await response1.json());
+    const response = await reqContext.get("/booking");
+    console.log(await response.json());
 
 })
 
-// baseURL
+// 3.baseURL passed with reqContext2
+
+test('API testing GET practice 3', async () => {
+
+    const response = await reqContext2.get("/booking");
+    console.log(await response.json());
+
+})
+
+// 4.baseURL in playwright confing
+
+test('API testing GET practice 4', async ({request}) => {
+
+    const response = await request.get("/booking");
+    console.log(await response.json());
+
+})
