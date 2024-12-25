c1fc8503ff618107d0fb39899869630972c35069
Marko <markobattlenet@gmail.com>
2024-12-25 14:10:14 +0100

    Different ways to pass Headers

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,6 +31,9 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
     baseURL: "https://restful-booker.herokuapp.com",
+    extraHTTPHeaders: {
+      Accept: "application/json"
+    },
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     screenshot: 'on',
     video: 'retain-on-failure',
--- tests/APITestingGet.spec.ts
+++ tests/APITestingGet.spec.ts
@@ -1,26 +1,36 @@
 import { test, request } from "@playwright/test";
 
-// 3. baseURL before all
+// 3. baseURL before all, new Context, extraHTTPHeaders
 let reqContext2
 test.beforeAll("Before all the tests...", async () => {
     reqContext2 = await request.newContext({
-        baseURL: "https://restful-booker.herokuapp.com"
+        baseURL: "https://restful-booker.herokuapp.com",
+        extraHTTPHeaders: {
+            Accept: "application/json"
+        }
     })
 })
 
 
-// 1.baseURL passed to http method, API RequestContext
+// 1.baseURL passed to http method, API RequestContext, passing header
 test('API testing GET practice 1', async ({ request }) => {
 
-    const response = await request.get("https://restful-booker.herokuapp.com/booking");
+    const response = await request.get("https://restful-booker.herokuapp.com/booking", {
+        headers: {
+            Accept: "application/json"
+        }
+    });
     console.log(await response.json());
 
 })
 
-// 2.baseURL passed with NewContext , API Request
+// 2.baseURL passed with NewContext , API Request, extraHTTPHeaders
 test('API testing GET practice 2', async () => {
     const reqContext = await request.newContext({
-        baseURL: "https://restful-booker.herokuapp.com"
+        baseURL: "https://restful-booker.herokuapp.com",
+        extraHTTPHeaders: {
+            Accept: "application/json"
+        }
     });
     const response = await reqContext.get("/booking");
     console.log(await response.json());
@@ -28,7 +38,6 @@ test('API testing GET practice 2', async () => {
 })
 
 // 3.baseURL passed with reqContext2
-
 test('API testing GET practice 3', async () => {
 
     const response = await reqContext2.get("/booking");
@@ -36,9 +45,8 @@ test('API testing GET practice 3', async () => {
 
 })
 
-// 4.baseURL in playwright confing
-
-test('API testing GET practice 4', async ({request}) => {
+// 4.baseURL, extraHTTPHeaders in playwright confing
+test('API testing GET practice 4', async ({ request }) => {
 
     const response = await request.get("/booking");
     console.log(await response.json());
