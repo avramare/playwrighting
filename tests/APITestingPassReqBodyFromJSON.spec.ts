9e695cb54ab8cf97f3377eaeabdc8184205c8f4e
Marko <markobattlenet@gmail.com>
2024-12-28 16:05:13 +0100

    Pass Request Body from JSON for POST Call

--- tests/APITestingPassReqBodyFromJSON.spec.ts
+++ tests/APITestingPassReqBodyFromJSON.spec.ts
@@ -4,34 +4,13 @@ import ApiJson from "../testdata/apidata.json"
 test('API Testing - Pass Request Body from JSON for POST Call', async ({ request }) => {
 
     const responsePost = await request.post("/booking", {
-        data:
-        {
-            "firstname": "Jim",
-            "lastname": "Brown",
-            "totalprice": 111,
-            "depositpaid": true,
-            "bookingdates": {
-                "checkin": "2018-01-01",
-                "checkout": "2019-01-01"
-            },
-            "additionalneeds": "Breakfast"
-        }
+        data: ApiJson
     })
 
     const responsePostJson = await responsePost.json();
-    expect(responsePostJson.booking).toMatchObject({
-        "firstname": "Jim",
-        "lastname": "Brown",
-        "totalprice": 111,
-        "depositpaid": true,
-        "bookingdates": {
-            "checkin": "2018-01-01",
-            "checkout": "2019-01-01"
-        },
-        "additionalneeds": "Breakfast"
-    });
+    expect(responsePostJson.booking).toMatchObject(ApiJson);
 
-    expect(responsePostJson.booking.additionalneeds).toEqual("Breakfast");
+    expect(responsePostJson.booking.additionalneeds).toEqual(ApiJson.additionalneeds);
 
 })
 
