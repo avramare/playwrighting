57d28480817545059e9e46ece8117268ce5670e3
Marko <markobattlenet@gmail.com>
2024-12-28 16:31:01 +0100

    API Testing - Pass Request Payload from JSON for Put Call

--- tests/APITestingPassReqBodyFromJSON.spec.ts
+++ tests/APITestingPassReqBodyFromJSON.spec.ts
@@ -4,16 +4,24 @@ import ApiJson from "../testdata/apidata.json"
 test('API Testing - Pass Request Body from JSON for POST Call', async ({ request }) => {
 
     const responsePost = await request.post("/booking", {
-        data: ApiJson
+        data: ApiJson.PostCallData
     })
 
     const responsePostJson = await responsePost.json();
-    expect(responsePostJson.booking).toMatchObject(ApiJson);
+    expect(responsePostJson.booking).toMatchObject(ApiJson.PostCallData);
 
-    expect(responsePostJson.booking.additionalneeds).toEqual(ApiJson.additionalneeds);
+    expect(responsePostJson.booking.additionalneeds).toEqual(ApiJson.PostCallData.additionalneeds);
 
 })
 
 test('API Testing - Pass Request Payload from JSON for Put Call', async ({ request }) => {
 
+    const responsePut = await request.put("/booking/1", {
+        data: ApiJson.PutCallData
+    });
+
+    const responsePutJson = await responsePut.json();
+    expect(responsePutJson).toMatchObject(ApiJson.PutCallData);
+
+    expect(responsePutJson.firstname).toEqual(ApiJson.PutCallData.firstname);
 })
